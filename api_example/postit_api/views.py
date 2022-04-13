d46284bdfed9cfaa75ee6325e7f530187bb20bfb
Donatas Noreika <donoras@gmail.com>
2022-04-13 19:16:17 +0300

    1 done

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post, PostLike, Comment, CommentLike
 from .serializers import PostSerializer
 
@@ -8,7 +8,14 @@ class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
