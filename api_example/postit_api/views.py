46799954dd4a50b96d38c1f884b0da8b04307f7f
Donatas Noreika <donoras@gmail.com>
2022-04-14 18:44:13 +0300

    2 - put, delete - done

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -3,6 +3,7 @@
 from rest_framework import generics, permissions
 from .models import Post, PostLike, Comment, CommentLike
 from .serializers import PostSerializer
+from rest_framework.exceptions import ValidationError
 
 class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
@@ -19,3 +20,23 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post:
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('Negalima trinti svetimų pranešimų!')
+
+    def put(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError('Negalima koreguoti svetimų pranešimų!')
